ed3b4239d94d9367d808be7cbf2947cd2f33d686
Alexander Sgibnev <sae762@gmail.com>
2018-01-19 12:53:44 +0600

    improvements for usability

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # mjs-clock-tm1637
 Advanced clock on esp8266+tm1637+MongooseOS+MJS
+
+- use wemos v2 mini or compatible, and tm1637 led with a colon
+- build and flash firmware (mos build --platform esp8266, mos flash)
+- initially clock not configured, blue led flashing fast
+- connect to wifi Clock_XXXXXX using password 12345678
+- go to http://192.168.4.1
+- enter your home network name (ssid)  and password
+- enter your timezone and desired brightness for led
+- press "Save... "
+- clock goes to reboot 
+- while not connecting to wifi, blue led blink slowly, wrong time displayed 
+- when connected to internet, blue led turns off, correct time displayed
+- in case of connection failure, time still counted, but blue led will blink slowly 
+- when connection restored, blue led turns off
+- you can reset controller to initial settings, by connecting pins D7 and D8. blue led turns on and clock goes to reboot
--- fs/init.js
+++ fs/init.js
@@ -4,33 +4,65 @@ load('api_gpio.js');
 load('api_net.js');
 load('api_sys.js');
 load('api_timer.js');
+load('api_mqtt.js'); //for internet testing )
 
-let led = 2;//Cfg.get('pins.led');
-let tz=6;//Cfg.get('clock.timezone');
-tz=tz*60*60;//hr *m *s
-
+//tm1637 functions, defined in main.c
 let setBr = ffi('void setBrightness(int, bool)');
 let showNum = ffi ('void showNumberDec(int, bool, int, int)');
 let showNumEx = ffi ('void showNumberDecEx(int, int, bool, int, int)');
 let setSeg = ffi ('void setSegments(void* , int, int)');
- 
-let getInfo = function() {
-  return JSON.stringify({
-    total_ram: Sys.total_ram(),
-    free_ram: Sys.free_ram()
-  });
-};
 
-setBr(2,true);
-let colon=0;
-// Blink built-in LED every second
+
+let led = 2;//Cfg.get('pins.led'); //LED pin
 GPIO.set_mode(led, GPIO.MODE_OUTPUT);
-Timer.set(1000 /* 1 sec */, true /* repeat */, function() {
-  let value = GPIO.toggle(led);
-  showNumEx(timeHM(), colon^=0xFF ,true , 4, 0);
-  //print(timeMS());
-  //print(value ? 'Tick' : 'Tock', 'uptime:', Sys.uptime(), getInfo());
+let br=Cfg.get('clock.brightness');  //display brightness
+let tz=Cfg.get('clock.timezone');//Timezone
+tz=tz*60*60;//hr *m *s
+let colon=0;//colon flag for colon blink
+let wifi_sta=false;// true only if sta configured
+let wifi_connected=false;// true only if ip obtained
+let internet_ok=false;
+
+//reset configuration, if short between pin D7 (13) and D8 (15)
+GPIO.set_mode(13, GPIO.MODE_OUTPUT);//13->0
+GPIO.write(13,0); 
+GPIO.set_button_handler(15, GPIO.PULL_UP, GPIO.INT_EDGE_NEG, 200, function() {
+    GPIO.write(led,0); //turns led on
+    Cfg.set({wifi: {ap: {enable: true}}}); 
+    Cfg.set({wifi: {sta: {enable: false}}}); 
+    Cfg.set({wifi: {sta: {password: ""}}}); 
+    Sys.reboot(100);
 }, null);
+
+setBr(br,true);//set brightness to 2 and turns led on (will effect after writing)
+
+
+//fast blink - is wifi client is not configured 
+if (!Cfg.get('wifi.sta.enable')) {
+  Timer.set(150 /* 0.3 sec */, true /* repeat */, function() {
+    GPIO.toggle(led);
+  }, null);
+} else {  
+  wifi_sta=true;
+}
+  
+//main cycle - show info and blink colon
+//slow blink if wifi problem
+Timer.set(1000 /* 1 sec */, true /* repeat */, function() {
+  if (wifi_sta)
+    if(!internet_ok) 
+      GPIO.toggle(led); 
+  Display();
+  }, null);
+
+function Display() {
+  if (!wifi_sta) {
+    //TODO: displsy "AP" ?
+    showNumEx(0, 0 ,true , 4, 0);
+    return;
+  }
+  showNumEx(timeHM(), colon^=0xFF ,true , 4, 0);
+}
 
 //returns digits for display, "09:01" will be 901 etc
 function timeHM() {
@@ -50,4 +82,24 @@ function timeMS() {
   }
   return t;
 }
+
+MQTT.setEventHandler(function(conn, ev, edata) {
+  if (ev !== 0) {
+    internet_ok=false;
+    print('*MQTT ev: ', ev);
+  }
+  if (ev === MQTT.EV_CONNACK) {
+    internet_ok=true;
+    GPIO.write(led,1); //turns led off
+  }}, null);
+
+
+// Monitor network connectivity.
+Event.addGroupHandler(Net.EVENT_GRP, function(ev, evdata, arg) {
+  if (ev!==0) wifi_connected=false;
+  if (ev === Net.STATUS_GOT_IP) {
+    wifi_connected=true;
+  }
+}, null);
+
 
